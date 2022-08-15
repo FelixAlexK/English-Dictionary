607a9f0bbe5a32e6370a017ae4acbeb372193a18
felix <fkuhbier@stud.hs-heilbronn.de>
2022-08-15 17:16:31 +0200

    added function

---
+++ main.js
@@ -0,0 +1,20 @@
+let dictionary = JSON.parse(localStorage.getItem("dictionary")) || {};
+
+function add(){
+    dictionary[deText.value] = enText.value;
+    deText.value = "";
+    enText.value = "";
+
+    localStorage.setItem("dictionary", JSON.stringify(dictionary));
+    render();
+}
+
+function render(){
+    dictionarylist.innerHTML = "";
+    for(let key in dictionary){
+
+        dictionarylist.innerHTML += `<li>${key} - ${dictionary[key]}<button class="del_btn">X</button></li>`;
+    }
+
+}
+
