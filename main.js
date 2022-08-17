ceb81767708659e3836f7775328381754bcfae18
felix <fkuhbier@stud.hs-heilbronn.de>
2022-08-17 16:56:50 +0200

    added function

--- main.js
+++ main.js
@@ -1,29 +1,59 @@
 let dictionary = JSON.parse(localStorage.getItem("dictionary")) || {};
-
 function add() {
 
-    
-    dictionary[deText.value] = enText.value;
-    deText.value = "";
-    enText.value = "";
+    try {
+        dictionary[deText.value] = enText.value;
+        deText.value = "";
+        enText.value = "";
 
-    localStorage.setItem("dictionary", JSON.stringify(dictionary));
-    render();
+        localStorage.setItem("dictionary", JSON.stringify(dictionary));
+        render();
+    } catch (error) {
+        handleError(error);
+    }
+
+
 }
 
-function render() {
-    dictionarylist.innerHTML = "";
-    for (let key in dictionary) {
+function resizeDictionary() {
 
-        dictionarylist.innerHTML += `<li>${key} - ${dictionary[key]}<button onclick="remove()" class="del_btn">X</button></li>`;
+    var main = document.getElementById('main').offsetHeight;
+    var sidebar = document.getElementById('dictionarylist').offsetHeight;
+    if (sidebar > main) {
+        main = sidebar;
+        document.getElementById('main').style.height = document.getElementById('dictionarylist').style.height = main + 'px'
+    } else {
+        sidebar = main;
+        document.getElementById('dictionarylist').style.height = document.getElementById('main').style.height = sidebar + 'px'
     }
 
 }
 
-function remove() {
+function render() {
+    try {
+        dictionarylist.innerHTML = "";
+        for (let key in dictionary) {
+
+            dictionarylist.innerHTML += `<li>${key} - ${dictionary[key]}</li>`;
+        }
+        itemsCount();
+    } catch (error) {
+        handleError(error);
+    }
+
 
 }
 
-function search() {
+function itemsCount() {
+    try {
+        let items = Object.keys(dictionary).length;
+        dictionaryItemsCount.innerHTML = `${items} entries`;
+    } catch (error) {
+        handleError(error);
+    }
 
+}
+
+function handleError(error) {
+    //alert("Error: " + error);
 }
